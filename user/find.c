e42a77b59e5e03422c6b0f7a1f5d6b83e2bf331b
shij <shijixttb@163.com>
2021-06-09 14:08:02 +0800

    support find pattern

--- user/find.c
+++ user/find.c
@@ -6,7 +6,9 @@
 
 #define PATH_MAX 1024
 
-void find(char *dirpath, char *filename)
+int match(char *, char *);
+
+void find(char *dirpath, char *pattern)
 {
   int fd;
   int n;
@@ -38,7 +40,7 @@ void find(char *dirpath, char *filename)
 
     strcpy(filepath + slen, de.name);
 
-    if (strcmp(de.name, filename) == 0)
+    if (match(pattern, de.name))
     {
       printf("%s\n", filepath);
     }
@@ -55,7 +57,7 @@ void find(char *dirpath, char *filename)
       {
         continue;
       }
-      find(filepath, filename);
+      find(filepath, pattern);
     }
   }
 
@@ -76,4 +78,47 @@ int main(int argc, char *argv[])
 
   find(argv[1], argv[2]);
   exit(0);
+}
+
+// Regexp matcher from Kernighan & Pike,
+// The Practice of Programming, Chapter 9.
+
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+int match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do
+  { // must look at empty string
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do
+  { // a * matches zero or more instances
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
 }
